c4e8da5bd449bc032a7668c307b476ff03b21244
alper yavuz ün <yavuzun50@hotmail.com>
2022-02-28 17:14:17 +0300

    MVC folders added

---
+++ ProjectIdCard/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectIdCard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectIdCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public int Index3()
+        {
+            return 24;
+        }
+
+        public IActionResult Index5()
+        {
+            var ktp = new List<Kitap>()
+            {
+                new Kitap(){ID=1, KitapAd="80 günde devri alem", Yazar="Jules Verne"},
+                new Kitap(){ID=2, KitapAd="Cesur yeni dünya", Yazar="Aldous Huxley"},
+                new Kitap(){ID=3, KitapAd="Ben Robot", Yazar="Isaac Asimov"}
+            };
+
+            return View(ktp);
+        }
+    }
+}
--- ProjectIdCard/Startup.cs
+++ ProjectIdCard/Startup.cs
@@ -16,6 +16,7 @@ namespace ProjectIdCard
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +29,20 @@ namespace ProjectIdCard
 
             app.UseRouting();
 
+            // for core version 2.0
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}"
+            //        );
+            //});
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}");
             });
         }
     }
